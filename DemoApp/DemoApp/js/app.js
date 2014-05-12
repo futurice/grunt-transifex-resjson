5f4637bf7299798b3f3a6a8f22d927f6721acd77
Antti Sykari <antti.sykari@futurice.com>
2014-05-12 14:19:28 +0300

    Add a simple demo app for testing

---
+++ DemoApp/DemoApp/js/app.js
@@ -0,0 +1,24 @@
+﻿
+(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/html/app.html", {
+        ready: function (element, options) {
+            var localizeButtons = document.querySelectorAll(".localize");
+            for (var i = 0; i < localizeButtons.length; i++) {
+                localizeButtons[i].addEventListener("click", process, false);
+            }
+        }
+    });
+
+    function setLanguage(lang) {
+        Windows.Globalization.ApplicationLanguages.primaryLanguageOverride = lang;
+    }
+
+    function process(args) {
+        var lang = args.target.dataset.language || "en-US";
+        setLanguage(lang);
+
+        WinJS.Resources.processAll();
+    }
+})();
